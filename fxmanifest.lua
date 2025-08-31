c3fadcb3b861dba47dd39de65bee2210a4e675e3
D7me Hub <213321243+D7meHub71@users.noreply.github.com>
2025-08-31 11:37:25 +0300

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54        'yes'
+
+name 'qb-carradio'
+
+author 'D71'
+description 'BY : https://github.com/D7meHub71 ©️'
+
+shared_scripts 'config.lua'
+
+client_scripts 'client.lua'
+
+server_scripts 'server.lua'
+
+dependencies 'xsound'
